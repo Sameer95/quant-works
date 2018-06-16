4a7efba930d6f56277b11e4909709282155080e4
Sameer95 <kadi.vinaysameerraja@gmail.com>
2018-06-16 22:06:21 +0530

    eff_front_v2_cvxopt

--- scripts/plot_eff_frontier_v2.py
+++ scripts/plot_eff_frontier_v2.py
@@ -2,6 +2,8 @@ import cPickle as pkl
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
+import cvxopt as opt
+from cvxopt import blas, solvers
 
 def expected_return(seq):
 	E_Ret = np.mean(seq)
@@ -117,25 +119,44 @@ def compute_covariance_nifty(mode):
 			assert cov[0][1] == cov[1][0]
 			covar_matrix[sec1][sec2] = cov[0][1]
 
-def calc_eff_frontier():
-	a = 1
-	return a
+def calc_eff_frontier(pbar, S):
+	n = len(pbar)
+	N = 100
+	mus = [10**(5.0 * t/N - 1.0) for t in range(N)]
+
+	# Create constraint matrices
+	G = -opt.matrix(np.eye(n))   # negative n x n identity matrix
+	h = opt.matrix(0.0, (n ,1))
+	A = opt.matrix(1.0, (1, n))
+	b = opt.matrix(1.0)	
+
+	portfolios = [solvers.qp(mu*S, -pbar, G, h, A, b)['x'] for mu in mus]
+
+	returns = [blas.dot(pbar, x) for x in portfolios]
+	risks = [np.sqrt(blas.dot(x, S*x)) for x in portfolios]
+
+	#m1 = np.polyfit(returns, risks, 2)
+	#x1 = np.sqrt(m1[2] / m1[0])
+
+	return returns,risks
 
 if __name__=="__main__":
 	np.random.seed(10)
-	global mode, data, data_path, num_combns
-	E_ret_risk = {}
-	num_combns = 100000
+	global mode, data, data_path, num_portfolios, covar_matrix
+	plot_individual_sec = True
+	plot_portfolio_comb = False
+	plot_efficient_frontier = True
 
 	data_path = "/home/vinay/Documents/quant-works/data/"
 	data = pkl.load(open(data_path+'nifty500.pkl','r'))
 
-	covar_matrix = np.zeros((len(data), len(data)))
+	E_ret_risk = {}
+	num_porfolios = 10000
+
 	compute_risk_var_nifty(2)
 
 	return_data = []
 	risk_data = []
-
 	idx = 0
 	for sym, val in E_ret_risk.iteritems():
 		return_data.append(val[0])
@@ -145,25 +166,36 @@ if __name__=="__main__":
 
 	return_data = np.asarray(return_data)[:, None]
 	risk_data = np.asarray(risk_data)
-	plot_data(risk_data, return_data)
-	print np.max(return_data)
 
+	covar_matrix = np.zeros((len(data), len(data)))
 	compute_covariance_nifty(2)
-	portfolio_return_data = []
-	portfolio_risk_data = []
-	portfolios = generate_samples(shape = (len(E_ret_risk), 1), n_samples = num_combns, distribution = 'binomial')
 
-	securities = data.keys()
-	sec_indices = np.arange(len(securities))
-	tuples = list(itertools.combinations(sec_indices, 2))
-	for portfolio in portfolios:
-		E_ret_p = np.dot(portfolio.T, return_data)
-		weight_combns = np.dot(portfolio, portfolio.T)
-		weighted_cov = covar_matrix*weight_combns
-		E_cov_p = np.sum(weighted_cov)
-		E_risk_p = np.sqrt(E_cov_p)
-		portfolio_return_data.append(E_ret_p)
-		portfolio_risk_data.append(E_risk_p)
+	return_data = opt.matrix(return_data)
+	covar_matrix = opt.matrix(covar_matrix)
 
-	plot_data(portfolio_risk_data, portfolio_return_data)
-	print np.max(portfolio_return_data)
+	if plot_individual_sec:
+		plot_data(risk_data, return_data)
+
+	if plot_portfolio_comb:
+		portfolio_return_data = []
+		portfolio_risk_data = []
+		portfolios = generate_samples(shape = (len(E_ret_risk), 1), n_samples = num_portfolios, distribution = 'binomial')
+
+		securities = data.keys()
+		sec_indices = np.arange(len(securities))
+		tuples = list(itertools.combinations(sec_indices, 2))
+
+		for portfolio in portfolios:
+			portfolio = opt.matrix(portfolio)
+			E_ret_p = np.dot(portfolio.T, return_data)
+			weight_combns = np.dot(portfolio, portfolio.T)
+			weighted_cov = covar_matrix*weight_combns
+			E_cov_p = np.sum(weighted_cov)
+			E_risk_p = np.sqrt(E_cov_p)
+			portfolio_return_data.append(E_ret_p)
+			portfolio_risk_data.append(E_risk_p)
+
+	if plot_efficient_frontier:
+		portfolio_return_data, portfolio_risk_data = calc_eff_frontier(return_data, covar_matrix)
+		plot_data(portfolio_risk_data, portfolio_return_data)
+		print np.max(portfolio_return_data)
